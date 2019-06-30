58b15033c861f5c35ce7a04701e60e944bf73a4a
MuhammadMahad <MohummadMahad@gmail.com>
2019-07-01 02:26:21 +0500

    test_backup

---
+++ Level_Design_Test/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed; //public variables ae available in inspector
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        change.Normalize();
+        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
